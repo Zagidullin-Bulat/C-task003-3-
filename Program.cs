e6f1be02885f07bcb52828a73596563e024a3eba
Zagidullin-Bulat <120476991+Zagidullin-Bulat@users.noreply.github.com>
2022-12-20 22:52:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введи число: ");
+int newNumber = Convert.ToInt32(Console.ReadLine());
+
+if (newNumber % 7 == 0)
+    if (newNumber % 23 == 0)
+        { 
+            Console.WriteLine ("Число делится на 7 и на 23!");
+        }
+    else
+        {
+            Console.WriteLine ("Число не делится на 23!");
+        }
+else if (newNumber % 23 == 0)
+    if (newNumber % 7 == 0)
+        { 
+            Console.WriteLine ("Число делится на 7 и на 23!");
+        }
+    else
+        {
+            Console.WriteLine ("Число не делится на 7!");
+        }
+else
+{
+    Console.WriteLine ("Числа не кратны 7 и 23!");
+}
